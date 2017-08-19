0f82818571d6cc7bb678fe3718ad9675e510fafc
Krazyman <narutoshippuden12_@hotmail.com>
2017-08-19 15:46:23 +1000

    poses

--- shenanigans/shenanigans.py
+++ shenanigans/shenanigans.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from random import *
 
 class Shenanigans:
     """Shenanigans - silly stuff for your bot"""
@@ -9,12 +10,12 @@ class Shenanigans:
     @commands.command(pass_context=True)
     async def boop(self, ctx):
         """boooooop"""
-        await self.bot.say('boop')
+        await self.bot.say('https://i.imgur.com/PLwj35n.jpg')
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True) #@command means that this is the command 
     async def double(self, ctx, number: int):
         """doubles what you give me"""
-        pass
+        await self.bot.say(number*2)
 
     @commands.command(pass_context=True, no_pm=True)
     async def add(self, ctx, a: int, b: int):
@@ -29,7 +30,9 @@ class Shenanigans:
     @commands.command(pass_context=True)
     async def pose(self, ctx):
         """strikes a random pose!"""
-        pass
+        poses = [":SeemsMarco:", ":MarcoSalt:", ":MarcoSaltier:"]
+        await self.bot.say(poses[randrange([1, 4])])
+        
 
 def setup(bot):
     bot.add_cog(Shenanigans(bot))
