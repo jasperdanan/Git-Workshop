cb377da4798acd78f8664e1f30d34a357a18c03b
Nathan <njpaul_1@yahoo.com>
2017-08-19 15:46:09 +1000

    Auto stash before merge of "master" and "jacks/master"

--- shenanigans/shenanigans.py
+++ shenanigans/shenanigans.py
@@ -14,12 +14,17 @@ class Shenanigans:
     @commands.command(pass_context=True)
     async def double(self, ctx, number: int):
         """doubles what you give me"""
-        await self.bot.say('double it')
+        await self.bot.say(number*2)
         pass
 
     @commands.command(pass_context=True, no_pm=True)
     async def add(self, ctx, a: int, b: int):
+        
         """add 2 numbers"""
+        a = 5
+        b = 4
+        ctx = a + b
+        await self.bot.say(ctx )
         pass
 
     @commands.command(pass_context=True)
